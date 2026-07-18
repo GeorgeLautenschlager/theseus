e80cae1bd7cd7956f266170e0a31ea13f845147a
George Lautenschlager <GeorgeLautenschlager@gmail.com>
2026-07-18 15:23:43 -0400

    Fix up Alty

--- src/theseus/agents/alty_mcgee.py
+++ src/theseus/agents/alty_mcgee.py
@@ -1,6 +1,8 @@
-from theseus.chat_effector import ChatEffector
+from theseus.stimulus_log import StimulusLog
 from theseus.cognitive_core import CognitiveCore
-from theseus.model_providers.openrouter_provider import OpenRouterProvider
+from theseus.chat_observer import ChatObserver
+from theseus.chat_effector import ChatEffector
+from theseus.model_providers.lm_studio_provider import LmStudioProvider
 
 ALTY_CONSTITUTION = """You are the crash test dummy of Theseus Agents.
     You will be instantiated in tests, in development and anywhere else we need a stand-in.
@@ -10,14 +12,27 @@ ALTY_CONSTITUTION = """You are the crash test dummy of Theseus Agents.
 
 class AltyMcGee:
     def __init__(self):
-        self.stimulus_log = StimulusLog()
+        chat_effector = ChatEffector()
+        self.stimulus_log = StimulusLog('stimulus_log.jsonl')
         self.core = CognitiveCore(
             stimulus_log=self.stimulus_log,
             constitution=ALTY_CONSTITUTION,
-            model_providers=OpenRouterProvider(model="google/gemma-4-31b-it"),
-            effectors=ChatEffector(),
+            model_providers=[LmStudioProvider(model="gemma-4-e4b-it-qat-nvfp4")],
+            effectors={chat_effector.name: chat_effector},
             name="Alty McGee",
         )
         self.chat_observer = ChatObserver(
             stimulus_log=self.stimulus_log,
+            orient_chat_message_callback=self.core.orient
         )
+    
+    def run(self):
+        """Run the agent. This is the main entry point for the agent."""
+        while True:
+            self.chat_observer.observe_chat_message()
+
+def main() -> None:
+    AltyMcGee().run()
+
+if __name__ == "__main__":
+    main()
